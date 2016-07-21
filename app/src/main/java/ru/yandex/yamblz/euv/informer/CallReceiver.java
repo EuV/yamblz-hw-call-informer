3491fba767f8b70892cbd059489fe0f65e95e0e7
Evgenij Vasilev <vasil.ev.genij@gmail.com>
2016-07-21 20:06:05 +0300

    Add Service & alert window

--- app/src/main/java/ru/yandex/yamblz/euv/informer/CallReceiver.java
+++ app/src/main/java/ru/yandex/yamblz/euv/informer/CallReceiver.java
@@ -3,22 +3,22 @@ package ru.yandex.yamblz.euv.informer;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import static android.telephony.TelephonyManager.ACTION_PHONE_STATE_CHANGED;
 import static android.telephony.TelephonyManager.EXTRA_INCOMING_NUMBER;
 import static android.telephony.TelephonyManager.EXTRA_STATE;
 import static android.telephony.TelephonyManager.EXTRA_STATE_RINGING;
+import static ru.yandex.yamblz.euv.informer.InformerService.EXTRA_PHONE_NUMBER;
 
 public class CallReceiver extends BroadcastReceiver {
-    private static String TAG = CallReceiver.class.getSimpleName();
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (!intent.getAction().equals(ACTION_PHONE_STATE_CHANGED)) return;
         if (!intent.getStringExtra(EXTRA_STATE).equals(EXTRA_STATE_RINGING)) return;
 
-        String phoneNumber = intent.getStringExtra(EXTRA_INCOMING_NUMBER);
-        Log.e(TAG, "Incoming call: " + phoneNumber);
+        Intent informer = new Intent(context, InformerService.class);
+        informer.putExtra(EXTRA_PHONE_NUMBER, intent.getStringExtra(EXTRA_INCOMING_NUMBER));
+        context.startService(informer);
     }
 }
--- app/src/main/java/ru/yandex/yamblz/euv/informer/MainActivity.java
+++ app/src/main/java/ru/yandex/yamblz/euv/informer/MainActivity.java
@@ -1,12 +1,30 @@
 package ru.yandex.yamblz.euv.informer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import static ru.yandex.yamblz.euv.informer.InformerService.EXTRA_PHONE_NUMBER;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // For debug purposes only
+        Button button = new Button(this);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent informer = new Intent(MainActivity.this, InformerService.class);
+                informer.putExtra(EXTRA_PHONE_NUMBER, "+79999999999");
+                startService(informer);
+            }
+        });
+        setContentView(button);
     }
 }
